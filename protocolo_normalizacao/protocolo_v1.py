9695e74af617cff25fb4054736a103c6486cf0ed
Kaiih <kailane.saidel03@gmail.com>
2025-09-05 10:38:19 -0300

    Esqueleto de funcionamento do código

--- protocolo_normalizacao/protocolo_v1.py
+++ protocolo_normalizacao/protocolo_v1.py
@@ -1,3 +1,31 @@
 from opentrons import protocol_api
 from opentrons.protocol_api import ProtocolContext
 import csv
+
+metadata = {
+    'protocolName': 'Normalização de DNA',
+    'author': 'Kailane',
+    'description': 'Leitura de CSV, calculo de volumes de diluição, normalização e criação de relatório',
+    'apiLevel': '2.15'}
+
+class Normalizacao(ProtocolContext):
+    def __init__(self, protocol: ProtocolContext):
+        self.protocol = protocol
+        self.target_conc = 30.0  # ng/µL
+        self.final_volume = 20.0  # µL
+
+    def run(self,ctx):
+        pass
+    
+    def load_data(self):
+        pass
+    
+    def calculate_volumes(self, original_conc):
+        pass
+
+    def transfer_tris(self):
+        pass
+
+    def transfer_dna(self):
+        pass
+
